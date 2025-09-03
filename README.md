20456c6b60d69f85089078b3b35b88b451659e69
samuko-things <samuel.c.agba@gmail.com>
2025-09-03 21:37:12 +0100

    first working commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Easy IMU Version 2 (EIMU_V2) Arduino I2C Lib
+This library helps communicate with the already setup **`Easy IMU`** with  arduino microcontroller projects via I2C
+
+> you can use it in your Arduino-based robotics project (e.g Arduino UNO, Arduino NANO, Arduino MEGA, Esp32, etc.)
+
+A simple way to get started is simply to try out and follow the example code
+
+
+## How to Use the Library
+- Ensure you have the **`Easy IMU`**
+
+- Download download the library by clicking on the green Code button above (or clone it)
+  > if you download it, extract it and change the folder name to `epmc_arduino`
+
+- Move the downloaded library file to your Arduino library folder
+  > e.g on linux: ... home/Arduino/libraries/
+  >
+  > e.g on windows: ... Documents/Arduino/libraries/
+  >
+  > (or any where your arduino libraries are stored)
+
+- restart your ArduinoIDE and navigate to examples and run the example code and see how to control the motors.
+
+- you can copy this example code into your project and modify it to your taste.
---
+++ eimuV2.cpp
@@ -0,0 +1,134 @@
+#include "eimuV2.h"
+
+void clearI2CBuffer() {
+  while (Wire.available()) {
+    Wire.read();  // discard all leftover bytes
+  }
+}
+
+EIMU_V2::EIMU_V2(int slave_addr)
+{
+  slaveAddr = slave_addr;
+}
+
+float EIMU_V2::readQuat(int pos_no)
+{
+  float quat = get("/quat", pos_no);
+  return quat;
+}
+
+float EIMU_V2::readRPY(int pos_no)
+{
+  float rpy = get("/rpy", pos_no);
+  return rpy;
+}
+
+float EIMU_V2::readRPYVariance(int pos_no)
+{
+  float rpy_var = get("/rpy-var", pos_no);
+  return rpy_var;
+}
+
+float EIMU_V2::readAcc(int pos_no)
+{
+  float acc = get("/acc", pos_no);
+  return acc;
+}
+
+float EIMU_V2::readAccVariance(int pos_no)
+{
+  float acc_var = get("/acc-var", pos_no);
+  return acc_var;
+}
+
+float EIMU_V2::readGyro(int pos_no)
+{
+  float gyro = get("/gyro", pos_no);
+  return gyro;
+}
+
+float EIMU_V2::readGyroVariance(int pos_no)
+{
+  float gyro_var = get("/gyro-var", pos_no);
+  return gyro_var;
+}
+
+float EIMU_V2::readMag(int pos_no)
+{
+  float acc = get("/mag", pos_no);
+  return acc;
+}
+
+bool EIMU_V2::setWorldFrameId(int id=1)
+{
+  return send("/frame-id", -1, id);
+}
+
+int EIMU_V2::getWorldFrameId()
+{
+  float id = get("/frame-id", -1);
+  return (int)id;
+}
+
+float EIMU_V2::getFilterGain()
+{
+  float gain = get("/gain", -1);
+  return gain;
+}
+
+float EIMU_V2::get(String cmd_route, int motor_no)
+{
+  String msg_buffer = cmd_route;
+  msg_buffer += ",";
+  msg_buffer += String(motor_no);
+
+  masterSendData(msg_buffer);
+  masterReceiveData();
+  String dataMsg = masterReceiveData();
+
+  float val = dataMsg.toFloat();
+  return val;
+}
+
+bool EIMU_V2::send(String cmd_route, int motor_no, float val)
+{
+  String msg_buffer = cmd_route;
+  msg_buffer += ",";
+  msg_buffer += String(motor_no);
+  msg_buffer += ",";
+  msg_buffer += String(val, 3);
+
+  masterSendData(msg_buffer);
+  masterReceiveData();
+  String data = masterReceiveData();
+  if (data == "1")
+    return true;
+  else
+    return false;
+}
+
+void EIMU_V2::masterSendData(String i2c_msg)
+{
+  Wire.beginTransmission(slaveAddr);
+  Wire.print(i2c_msg);
+  Wire.endTransmission(true);
+}
+
+String EIMU_V2::masterReceiveData()
+{
+  String i2c_msg = "";
+  Wire.flush();
+  uint8_t dataSizeInBytes = Wire.requestFrom(slaveAddr, 15);
+  for (int i = 0; i < dataSizeInBytes; i += 1)
+  {
+    char c = Wire.read();
+    i2c_msg += c;
+  }
+  int indexPos = i2c_msg.indexOf((char)255);
+  if (indexPos != -1)
+  {
+    return i2c_msg.substring(0, indexPos);
+  }
+  i2c_msg.trim();
+  return i2c_msg;
+}
